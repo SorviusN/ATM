d07b2d08e6cbd2990c7bf1da03a97e26bd5bb74e
SorviusN <Opt1206@gmail.com>
2021-07-13 20:44:45 -0700

    All Files complete

--- XUnitTesting/UnitTest1.cs
+++ XUnitTesting/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using Lab02-ATM;
 using Xunit;
 
 namespace XUnitTesting
@@ -6,9 +7,21 @@ namespace XUnitTesting
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ReturnsBalance()
         {
+            Assert.Equal(Lab02-ATM.Balance, Lab02-ATM.ViewBalance());
+        }
 
+        [Fact]
+        public void ReturnsWithdrawalAmount()
+        {
+            Assert.Equal(20, Lab02-ATM.Withdraw(20));
+        }
+
+        [Fact]
+        public void ReturnsBalanceAmount()
+        {
+            Assert.Equal(20, Lab02-ATM.Deposit(20));
         }
     }
 }
