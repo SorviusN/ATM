357b7051a91c1b5196c1b4a1649092bca29d6b35
SorviusN <Opt1206@gmail.com>
2021-07-13 18:30:56 -0700

    Partially complete - switch statement and methods

--- Lab02-ATM/Program.cs
+++ Lab02-ATM/Program.cs
@@ -6,7 +6,35 @@ namespace Lab02_ATM
     {
         static void Main(string[] args)
         {
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
 
+        public static void InitiateProgram()
+        {
+            Decimal balance = 0;
+            string input = "";
+
+            while (input != "x")
+            {
+                Console.WriteLine(
+                    "Welcome to Harborstone. What would you like to do today?\n" +
+                    "");
+                switch(input.ToLower())
+                    {
+                        case "a":
+                            break;
+                        default:
+                            Console.WriteLine("Input not found. Please try again with correct input.");
+                            break;
+                    }
+
+            }
         }
     }
 }
